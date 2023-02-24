ca169f6dff592e5b269658b24676200ac08204bb
CamrenBell <cjb0132@yahoo.com>
2023-02-24 16:24:52 -0700

    updated requrments

--- deco_treasures_front_end/src/routes/ItemPage.jsx
+++ deco_treasures_front_end/src/routes/ItemPage.jsx
@@ -1,13 +1,19 @@
 import { useParams } from 'react-router-dom';
+import React, {useState} from 'react';
 
 export default function ItemPage() {
 
     let { itemID } = useParams();
+    const [data, setData] = useState(1);
+
+    fetch(`http://127.0.0.1:8000/inventory_api/${itemID}`)
+        .then(response => response.json())
+        .then(data => setData(data))
 
     return (
         <div>
           <h1>Deco Treasures</h1>
-          <p>{itemID}</p>
+          <p>{data}</p>
         </div>
     );
   }
